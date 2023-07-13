6d3e188594c991ae84deece1d0a7d26d5445a66e
milkiyd <milkiyd@bugger.com>
2023-07-13 15:42:15 +0000

    Add User card

---
+++ lib/views/widgets/user_card.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:randomuser/models/schemas/user.dart';
+
+class UserCard extends StatelessWidget {
+  const UserCard({super.key, required this.user});
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(
+        color: Colors.deepPurple[50],
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+              user.picture,
+            ),
+            radius: 20,
+          ),
+          const SizedBox(width: 12),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "${user.firstName} ${user.lastName}",
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                user.username,
+                style: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
